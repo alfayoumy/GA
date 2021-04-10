79aa04527accedc45a5fc8fb0bcc9ca1db62385f
alfayoumy <64507050+alfayoumy@users.noreply.github.com>
2021-04-10 15:19:46 +0200

    Create q3.sql

---
+++ q3.sql
@@ -0,0 +1,14 @@
+/*QUERY#1: HOW MANY CLICKS ARETHERE PER LANDING PAGE?*/
+SELECT
+  hits.page.pagePath AS LandingPage,
+  COUNT(totals.hits) AS Clicks
+FROM
+  `bigquery-public-data.google_analytics_sample.ga_sessions_*`,
+  UNNEST(hits) AS hits
+WHERE
+  hits.type="PAGE"
+  AND hits.hitNumber IS NOT NULL
+GROUP BY
+  LandingPage
+ORDER BY
+  Clicks DESC
